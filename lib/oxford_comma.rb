5471cdefa92054e5ff51a91158dc25b10b71e431
Linzell Godfrey <linzell.godfrey@live.co.uk>
2018-11-30 17:27:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
-end
+  if array.length == 1 
+    return array.join
+  end
+  elsif array.length == 2 
+    array.insert(1, " and ")
+    return array.join
+  end
+ else array.length >= 3
+   
+   
